9d069b8a85f69b69d6be97912baade136d14fdf7
Mikhail Samoylov <>
2020-12-04 18:04:14 +0300

    Fix version.

--- molecule/ppg/tests/versions/pgaudit.py
+++ molecule/ppg/tests/versions/pgaudit.py
@@ -7,7 +7,7 @@ pgaudit = {"ppg-11.5": {"version": "1.3"},
            "ppg-12.2": {"version": "1.4"},
            "ppg-12.3": {"version": "1.4"},
            "ppg-12.4": {"version": "1.4"},
-           "ppg-12.5": {"version": "1.5"},
+           "ppg-12.5": {"version": "1.4.1"},
            "ppg-13.0": {"version": "1.5"},
            "ppg-13.1": {"version": "1.5"}
            }
