7fa92618396700293536ace6f35ba83f647750e1
Mikhail Samoylov <>
2020-12-04 09:00:59 +0300

    PPG12.5. Add version.

--- molecule/ppg/tests/versions/pgaudit.py
+++ molecule/ppg/tests/versions/pgaudit.py
@@ -3,9 +3,11 @@ pgaudit = {"ppg-11.5": {"version": "1.3"},
            "ppg-11.7": {"version": "1.4.0"},
            "ppg-11.8": {"version": "1.4.0"},
            "ppg-11.9": {"version": "1.4.0"},
+           "ppg-11.10": {"version": "1.4.0"},
            "ppg-12.2": {"version": "1.4"},
            "ppg-12.3": {"version": "1.4"},
            "ppg-12.4": {"version": "1.4"},
+           "ppg-12.5": {"version": "1.4"},
            "ppg-13.0": {"version": "1.5"},
            "ppg-13.1": {"version": "1.5"}
            }
