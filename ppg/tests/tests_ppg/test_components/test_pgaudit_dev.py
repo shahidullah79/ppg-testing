18e4e246c9f91a85080b899e09d6b4db519ea8ac
Mikhail Samoylov <>
2021-04-02 16:34:37 +0300

    Add tests

--- ppg/tests/tests_ppg/test_components/test_pgaudit_dev.py
+++ ppg/tests/tests_ppg/test_components/test_pgaudit_dev.py
@@ -11,6 +11,8 @@ def test_pgaudit(host):
     with host.sudo("postgres"):
         result = host.run("cd /tmp/pg_audit && make installcheck USE_PGXS=1")
         if result.rc != 0:
+            print(result.stderr)
+            print(result.stdout)
             print(host.file("/tmp/pg_audit/regression.diffs").content)
             raise AssertionError
 
