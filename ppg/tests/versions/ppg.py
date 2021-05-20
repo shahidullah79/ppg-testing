6d405b32966dcef182df7fed994082554612191b
Mikhail Samoylov <>
2021-05-20 17:57:14 +0300

    Fix

--- ppg/tests/versions/ppg.py
+++ ppg/tests/versions/ppg.py
@@ -332,7 +332,7 @@ def get_ppg_versions(distro_type):
                                           distros=DISTROS, distro_type=distro_type),
             "ppg-12.6": get_pg12_versions(packages=["2:12.6-2", "12+225-1", '225-1'],
                                           distros=DISTROS, distro_type=distro_type),
-            "ppg-12.7": get_pg12_versions(packages=["2:12.7-2", "12+226-1", '226-1'],
+            "ppg-12.7": get_pg12_versions(packages=["2:12.7-2", "12+225-1", '225-1'],
                                           distros=DISTROS, distro_type=distro_type),
             "ppg-13.0": get_pg13_versions(packages=["2:13-0.1", "13+221-1", '221-1'],
                                           distros=DISTROS, distro_type=distro_type),
