2799e6d7abeb4501ac67b89318d20ba1f0741fc5
Mikhail Samoylov <>
2021-06-13 21:34:59 +0300

    Fix

--- patroni/tests/test_patroni.py
+++ patroni/tests/test_patroni.py
@@ -12,7 +12,6 @@ def test_patroni(host):
     assert host.service("patroni1").is_running
     assert host.service("patroni2").is_running
     assert host.service("etcd").is_running
-    assert host.service("haproxy").is_running
     with host.sudo("postgres"):
         select = 'cd && psql --host 127.0.0.1 --port 5000 postgres -c "select version()"'
         result = host.run(select)
