78f507a0a688c76d5987c57ca9b9dce0290cc90f
Mikhail Samoylov <>
2021-06-13 21:14:25 +0300

    Fix

--- patroni/tests/test_patroni.py
+++ patroni/tests/test_patroni.py
@@ -9,9 +9,12 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 def test_patroni(host):
     assert host.service("patroni").is_running
+    assert host.service("patroni1").is_running
+    assert host.service("patroni2").is_running
     assert host.service("etcd").is_running
+    assert host.service("haproxy").is_running
     with host.sudo("postgres"):
-        select = 'cd; psql --host 127.0.0.1 --port 5000 postgres -c "select version()"'
+        select = 'cd && psql --host 127.0.0.1 --port 5000 postgres -c "select version()"'
         result = host.run(select)
         print(result.stdout)
         assert result.rc == 0, result.stderr
