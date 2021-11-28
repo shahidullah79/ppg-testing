62b524a47306c3ba26898492ede6591ff1b1d175
Mikhail Samoylov <mikhail.samoylov@percona.com>
2021-11-28 18:50:28 +0300

    12.9 and 13.14 versions

--- ppg/tests/versions/pgbadger.py
+++ ppg/tests/versions/pgbadger.py
@@ -1,9 +1,11 @@
 pgbadger = {
     "11.12": {"version": "11.5", "binary_version": "pgBadger version 11.5"},
     "11.13": {"version": "11.5", "binary_version": "pgBadger version 11.5"},
+    "11.14": {"version": "11.6", "binary_version": "pgBadger version 11.6"},
     "12.6": {"version": "11.5", "binary_version": "pgBadger version 11.5"},
     "12.7": {"version": "11.5", "binary_version": "pgBadger version 11.5"},
     "12.8": {"version": "11.5", "binary_version": "pgBadger version 11.5"},
+    "12.9": {"version": "11.6", "binary_version": "pgBadger version 11.6"},
     "13.2": {"version": "11.5", "binary_version": "pgBadger version 11.5"},
     "13.3": {"version": "11.5", "binary_version": "pgBadger version 11.5"},
     "13.4": {"version": "11.5", "binary_version": "pgBadger version 11.5"},
--- ppg/tests/versions/pgbouncer.py
+++ ppg/tests/versions/pgbouncer.py
@@ -1,9 +1,11 @@
 pgbouncer = {
     "11.12": {"version": "1.15.0", "binary_version": "1.15.0"},
     "11.13": {"version": "1.16.0", "binary_version": "1.16.0"},
+    "11.14": {"version": "1.16.1", "binary_version": "1.16.1"},
     "12.6": {"version": "1.15.0", "binary_version": "1.15.0"},
     "12.7": {"version": "1.15.0", "binary_version": "1.15.0"},
     "12.8": {"version": "1.16.0", "binary_version": "1.16.0"},
+    "12.9": {"version": "1.16.1", "binary_version": "1.16.1"},
     "13.2": {"version": "1.15.0", "binary_version": "1.15.0"},
     "13.3": {"version": "1.15.0", "binary_version": "1.15.0"},
     "13.4": {"version": "1.16.0", "binary_version": "1.16.0"},
