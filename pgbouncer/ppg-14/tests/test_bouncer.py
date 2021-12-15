faf10c2b8a8b57cf7645300640f4642c1ee9bd93
Mikhail Samoylov <mikhail.samoylov@percona.com>
2021-12-15 16:21:04 +0300

    Fix

--- pgbouncer/ppg-14/tests/test_bouncer.py
+++ pgbouncer/ppg-14/tests/test_bouncer.py
@@ -9,7 +9,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 def test_pgbouncer(host):
     with host.sudo("postgres"):
-        result = host.run('PATH="/usr/pgsql-13/bin/:$PATH" && cd /tmp/pgbouncer && make check')
+        result = host.run('PATH="/usr/pgsql-14/bin/:$PATH" && cd /tmp/pgbouncer && make check')
         print(result.stdout)
         if result.rc != 0:
             print(result.stderr)
