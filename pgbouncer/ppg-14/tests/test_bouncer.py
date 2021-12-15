82615c3119796d6b98220feaabec5c721c8b6744
Mikhail Samoylov <mikhail.samoylov@percona.com>
2021-12-15 16:20:39 +0300

    Add pgbouncer pg-14

---
+++ pgbouncer/ppg-14/tests/test_bouncer.py
@@ -0,0 +1,18 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_pgbouncer(host):
+    with host.sudo("postgres"):
+        result = host.run('PATH="/usr/pgsql-13/bin/:$PATH" && cd /tmp/pgbouncer && make check')
+        print(result.stdout)
+        if result.rc != 0:
+            print(result.stderr)
+            raise AssertionError
+
+
