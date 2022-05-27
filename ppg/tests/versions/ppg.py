069c38418c20f55a9fc9b25559bc0143c6877c96
Mikhail Samoylov <mikhail.samoylov@percona.com>
2022-05-27 20:40:29 +0300

    Add ubunty jammy

--- ppg/tests/versions/ppg.py
+++ ppg/tests/versions/ppg.py
@@ -1,6 +1,6 @@
 from .extensions import get_extensions_ppg11, get_extensions_ppg12, get_extensions_ppg13
 
-DISTROS = ['buster', 'stretch', 'bionic', 'focal', 'bullseye']
+DISTROS = ['buster', 'stretch', 'bionic', 'focal', 'bullseye', 'jammy']
 DEB116_PACKAGES_TEMPLATE = ["percona-postgresql-{}",
                             "percona-postgresql-client",
                             "percona-postgresql",
