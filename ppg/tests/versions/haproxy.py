a7a1d6ae260bc06deda595215f93205b3d36aed1
Mikhail Samoylov <mikhail.samoylov@percona.com>
2022-08-17 23:48:40 +0300

    Update ppg versions

--- ppg/tests/versions/haproxy.py
+++ ppg/tests/versions/haproxy.py
@@ -1,7 +1,11 @@
 haproxy = {
     "11.16": {"version": "2.5.6", "binary_version": "2.5.6"},
+    "11.17": {"version": "2.5.6", "binary_version": "2.5.6"},
     "12.11": {"version": "2.5.6", "binary_version": "2.5.6"},
+    "12.12": {"version": "2.5.6", "binary_version": "2.5.6"},
     "13.7": {"version": "2.5.6", "binary_version": "2.5.6"},
+    "13.8": {"version": "2.5.6", "binary_version": "2.5.6"},
     "14.3": {"version": "2.5.6", "binary_version": "2.5.6"},
     "14.4": {"version": "2.5.6", "binary_version": "2.5.6"},
+    "14.5": {"version": "2.5.6", "binary_version": "2.5.6"},
 }
