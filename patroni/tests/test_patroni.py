4ce51c8a83d26fc1b9ad636b6e8ba63cc3220129
Mikhail Samoylov <mikhail.samoylov@percona.com>
2022-08-19 22:27:42 +0300

    Debug for patroni test

--- patroni/tests/test_patroni.py
+++ patroni/tests/test_patroni.py
@@ -9,10 +9,10 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 def test_patroni(host):
     assert host.service("etcd").is_running
-    # assert host.service("patroni").is_running
-    assert host.service("patroni1").is_running
-    assert host.service("patroni1").is_running
-    assert host.service("patroni2").is_running
+    print(host.run("journalctl -xe").stdout)
+    assert host.service("patroni").is_running, print(host.run("systemctl status patroni").stdout)
+    assert host.service("patroni1").is_running, print(host.run("systemctl status patroni1").stdout)
+    assert host.service("patroni2").is_running, print(host.run("systemctl status patroni2").stdout)
     with host.sudo("postgres"):
         select = 'cd && psql --host 127.0.0.1 --port 5000 postgres -c "select version()"'
         result = host.run(select)
