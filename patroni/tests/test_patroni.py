fff8305fd952b85c63f3b65f7b3e01c7c7b80366
Mikhail Samoylov <mikhail.samoylov@percona.com>
2022-08-20 08:55:27 +0300

    patroni: Delete debug lines

--- patroni/tests/test_patroni.py
+++ patroni/tests/test_patroni.py
@@ -12,10 +12,6 @@ def test_patroni(host):
     assert host.service("patroni").is_running, print(host.run("systemctl status patroni").stdout)
     assert host.service("patroni1").is_running, print(host.run("systemctl status patroni1").stdout)
     assert host.service("patroni2").is_running, print(host.run("systemctl status patroni2").stdout)
-    print(host.run("ps aux | grep haproxy").stdout)
-    print(host.run("cat /etc/haproxy/haproxy.cfg").stdout)
-    print(host.run("ps aux | grep postgres").stdout)
-    # with host.sudo("postgres"):
     select = 'cd && psql --host localhost --port 5000 postgres -U postgres -c "select version()"'
     result = host.run(select)
     print(result.stdout)
