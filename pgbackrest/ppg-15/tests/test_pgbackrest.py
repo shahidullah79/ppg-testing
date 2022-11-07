c198325f958a8d568d6b1abb785cadd6eaccdcfb
Mikhail Samoylov <mikhail.samoylov@percona.com>
2022-11-07 22:40:55 +0300

    Pgbackrest tests debug

--- pgbackrest/ppg-15/tests/test_pgbackrest.py
+++ pgbackrest/ppg-15/tests/test_pgbackrest.py
@@ -9,7 +9,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 def test_pgbackrest(host):
     with host.sudo("postgres"):
-        result = host.run('cd /var/lib/postgresql/ && pgbackrest/test/test.pl --pgsql-bin=/usr/lib/postgresql/15/bin --no-valgrind --log-level-test-file=off --no-coverage-report --module=command --module=storage --vm-max=2 --vm=none --no-coverage')
+        result = host.run('cd /var/lib/postgresql/ && pgbackrest/test/test.pl --pgsql-bin=/usr/lib/postgresql/15/bin --no-valgrind --log-level-test-file=off --no-coverage-report --module=command --module=storage --vm-out --vm=none --no-coverage')
         print(result.stdout)
         if result.rc != 0:
             print(result.stderr)
