40943c83f0cd4c1ebdddd1f00ef4ed39cbd8e62d
Mikhail Samoylov <mikhail.samoylov@percona.com>
2022-11-11 23:48:20 +0300

    Refactoring for patroni tests

--- patroni/tests/test_patroni.py
+++ patroni/tests/test_patroni.py
@@ -8,27 +8,32 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_patroni(host):
+def test_patroni_service(host):
     assert host.service("etcd").is_running
     assert host.service("patroni").is_running, print(host.run("systemctl status patroni").stdout)
     assert host.service("patroni1").is_running, print(host.run("systemctl status patroni1").stdout)
     assert host.service("patroni2").is_running, print(host.run("systemctl status patroni2").stdout)
+
+
+def test_haproxy_connect(host):
     select = 'cd && psql --host localhost --port 5000 postgres -U postgres -c "select version()"'
     result = host.run(select)
     print(result.stdout)
     assert result.rc == 0, result.stderr
 
+
+def test_cluster_status(host):
     cluster_cmd = 'patronictl -c /var/lib/pgsql/patroni_test/postgresql1.yml list -f json'
     cluster_result = host.run(cluster_cmd)
     print(cluster_result.stdout)
     assert cluster_result.rc == 0, cluster_result.stderr
-
     cluster_json = json.loads(cluster_result.stdout)
     assert len(cluster_json) == 3, f"Must have 3 nodes in the cluster, but found {len(cluster_json)}"
-    assert cluster_json[0]['State'] == 'running', cluster_json[0]
-    assert cluster_json[1]['State'] == 'running', cluster_json[1]
-    assert cluster_json[2]['State'] == 'running', cluster_json[2]
+    for cluster in cluster_json:
+        assert cluster['State'] == 'running', cluster
 
+
+def test_haproxy_web(host):
     curl_cmd = 'curl http://localhost:7000'
     curl_result = host.run(curl_cmd)
     assert curl_result.rc == 0, curl_result.stderr
