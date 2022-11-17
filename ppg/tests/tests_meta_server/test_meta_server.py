43431a394761297545d705bf66c20c0b4d08eb30
Mikhail Samoylov <mikhail.samoylov@percona.com>
2022-11-17 10:14:24 +0300

    Add debian tests for meta server

--- ppg/tests/tests_meta_server/test_meta_server.py
+++ ppg/tests/tests_meta_server/test_meta_server.py
@@ -13,7 +13,7 @@ RPM_PACKAGES = [
     'percona-pgaudit', f'percona-pg_repack{settings.MAJOR_VER}', f'percona-wal2json{settings.MAJOR_VER}'
 ]
 DEB_PACKAGES = [
-    f'percona-postgresql-{settings.MAJOR_VER}-server', 'percona-postgresql-common',
+    f'percona-postgresql-{settings.MAJOR_VER}', 'percona-postgresql-common',
     f'percona-postgresql-contrib', f'percona-pg-stat-monitor{settings.MAJOR_VER}',
     f'percona-postgresql-{settings.MAJOR_VER}-pgaudit', f'percona-postgresql-{settings.MAJOR_VER}-repack',
     f'percona-postgresql-{settings.MAJOR_VER}-wal2json'
